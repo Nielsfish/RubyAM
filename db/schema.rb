a0420f7dbb18634cdd37141fac1b56ab822cda1a
Nielsfish <259687@student.pwr.edu.pl>
2022-12-16 18:31:29 +0000

    User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_16_180630) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_16_181912) do
   create_table "employees", force: :cascade do |t|
     t.integer "EmployeeID"
     t.string "Login"
@@ -25,6 +25,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_16_180630) do
     t.integer "PositionID"
     t.string "PositionName"
     t.integer "Salary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "Login"
+    t.integer "Password"
+    t.string "FirstName"
+    t.string "LastName"
+    t.integer "AccountType", default: 0
+    t.string "Email"
+    t.string "AccountNo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
